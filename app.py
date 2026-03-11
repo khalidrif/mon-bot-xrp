a5bad6b06b46042ac2284a83a276630e5660ecd7
khalidrif <khalid.rhif@gmail.com>
2026-03-11 13:02:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if "bots" not in st.session_state:
         2: {"id": 2, "actif": True, "p_achat": 1.350, "p_vente": 1.380, "mise": 15.0, "etape": "ATTENTE_ACHAT", "qty": 0.0, "gain_cumule": 0.0, "cycles": 0},
         3: {"id": 3, "actif": False, "p_achat": 1.380, "p_vente": 1.420, "mise": 15.0, "etape": "ATTENTE_ACHAT", "qty": 0.0, "gain_cumule": 0.0, "cycles": 0},
         4: {"id": 4, "actif": False, "p_achat": 1.420, "p_vente": 1.450, "mise": 15.0, "etape": "ATTENTE_ACHAT", "qty": 0.0, "gain_cumule": 0.0, "cycles": 0},
-        5: {"id": 5, "actif": False, "p_achat": 1.450, "p_vente": 1.500, "mise": 15.0, "etape": "ATTENTE_ACHAT", "qty": 0.0, "gain_cumule": 0.0, "cycles": 0},
+        5: {"id": 5, "actif": False, "p_achat": 1.389, "p_vente": 1.39, "mise": 10.0, "etape": "ATTENTE_ACHAT", "qty": 0.0, "gain_cumule": 0.0, "cycles": 0},
         6: {"id": 6, "actif": False, "p_achat": 1.38, "p_vente": 1.39, "mise": 10.0, "etape": "ATTENTE_ACHAT", "qty": 0.0, "gain_cumule": 0.0, "cycles": 0},
 
     }
@@ -138,4 +138,5 @@ for i in sorted(st.session_state.bots.keys()):
 st.divider()
 for m in reversed(st.session_state.logs[-10:]):
     st.write(m)
+
 
